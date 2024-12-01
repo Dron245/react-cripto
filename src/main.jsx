e1f3b18fe2f3b714d2c81623e1fc9c276721abb9
Dron245 <Dron245@yandex.ru>
2024-12-01 18:53:21 +0300

    0112-1853

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import './index.scss'
+
+const root = document.getElementById('root')
+
+createRoot(root).render(<App />)
